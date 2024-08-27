3876d5d22d1bd051c96d743adf56a4f38f9b2ec4
Dylan Tin <dylant711@gmail.com>
2024-08-27 14:49:15 -0400

    finished arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(){
+    let num1= 2000;
+    let num2= 100;
+    return(num1 / num2);
+}
+
+const square = (parameter) => {return parameter * parameter;}
+
+const add = (parameter1, parameter2) => {return parameter1 + parameter2;}
